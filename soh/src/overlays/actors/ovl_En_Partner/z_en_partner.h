c94295c530924af9b08bb77e28ab64a09c740d35
Shishu the Dragon <183069616+ShishuTheDragon@users.noreply.github.com>
2025-07-06 17:39:29 +1200

    Ivan: alt hammer

--- soh/src/overlays/actors/ovl_En_Partner/z_en_partner.h
+++ soh/src/overlays/actors/ovl_En_Partner/z_en_partner.h
@@ -18,6 +18,7 @@ typedef struct EnPartner {
     Vec3s morphTable[15];
 
     ColliderCylinder collider;
+    ColliderCylinder weaponCollider;
 
     Color_RGBAf innerColor;
     Color_RGBAf outerColor;
