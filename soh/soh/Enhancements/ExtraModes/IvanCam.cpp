0a947252ce5b0e3812c87722b1aedb1c265c4415
Shishu the Dragon <183069616+ShishuTheDragon@users.noreply.github.com>
2026-01-31 15:04:02 +1300

    render framebuffer in window

--- soh/soh/Enhancements/ExtraModes/IvanCam.cpp
+++ soh/soh/Enhancements/ExtraModes/IvanCam.cpp
@@ -1,2 +1,32 @@
 #include "soh/Enhancements/ExtraModes/IvanCam.h"
 
+#include "fast/Fast3dWindow.h"
+#include "ship/Context.h"
+#include "soh/framebuffer_effects.h"
+
+#include <imgui.h>
+#include <memory>
+
+void IvanCamWindow::InitElement() {
+}
+
+void IvanCamWindow::UpdateElement() {
+}
+
+void IvanCamWindow::DrawElement() {
+    Fast::Fast3dWindow* window = (Fast::Fast3dWindow*)(&*Ship::Context::GetInstance()->GetWindow());
+
+    if (gIvanFrameBuffer < 0) {
+        ImGui::TextUnformatted("Ivan framebuffer not initialized.");
+        return;
+    }
+
+    uintptr_t fb = window->GetFramebufferTextureId(gIvanFrameBuffer);
+    if (!fb) {
+        ImGui::TextUnformatted("Ivan framebuffer unavailable.");
+        return;
+    }
+
+    ImVec2 avail = ImGui::GetContentRegionAvail();
+    ImGui::Image(reinterpret_cast<ImTextureID>(fb), avail);
+}
--- soh/soh/Enhancements/ExtraModes/IvanCam.h
+++ soh/soh/Enhancements/ExtraModes/IvanCam.h
@@ -4,8 +4,7 @@ class IvanCamWindow final : public Ship::GuiWindow {
   public:
     using GuiWindow::GuiWindow;
 
-    void InitElement() override{};
-    void DrawElement() override{};
-    // void Draw() override{};
-    void UpdateElement() override{};
+    void InitElement() override;
+    void DrawElement() override;
+    void UpdateElement() override;
 };
