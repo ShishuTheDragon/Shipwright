ffb8c4b2eba170392dd8c1f6f694cf7f17aeba80
Shishu the Dragon <183069616+ShishuTheDragon@users.noreply.github.com>
2026-02-06 16:09:56 +1300

    IvanEquipSlots: Separate equipment slots for Ivan

---
+++ soh/soh/Enhancements/ExtraModes/IvanSeparateEquipSlots.cpp
@@ -0,0 +1,248 @@
+#include "soh/Enhancements/game-interactor/GameInteractor_Hooks.h"
+#include "soh/ShipInit.hpp"
+
+#include "textures/parameter_static/parameter_static.h"
+#include "soh_assets.h"
+#include "functions.h"
+#include "macros.h"
+#include "variables.h"
+
+extern "C" {
+extern PlayState* gPlayState;
+extern GameInfo* gGameInfo;
+extern void* gItemIcons[158];
+extern const char* _gAmmoDigit0Tex[];
+Gfx* Gfx_TextureIA8(Gfx* displayListHead, void* texture, s16 textureWidth, s16 textureHeight, s16 rectLeft, s16 rectTop,
+                    s16 rectWidth, s16 rectHeight, u16 dsdx, u16 dtdy);
+}
+
+static const s16 cButtonSize = 16;
+static const s16 cButtonTexStep = 512 * 32 / cButtonSize;
+static const s16 itemIconSize = 16;
+static const s16 itemIconTexStep = 512 * 32 / itemIconSize;
+static const s16 itemSpacing = 16;
+static const s16 dPadSize = 32;
+static const s16 dPadTexStep = 512;
+static const s16 dPadCenterX = 64;
+static const s16 cButtonsCenterX = 256;
+static const s16 dPadCenterY = 208;
+static const s16 cButtonsCenterY = 208;
+static const s16 naviLabelYOffset = 4;
+static const s16 naviLabelXOffset = 8;
+
+enum class IvanItemIndex : u8 {
+    CLeft = 0,
+    CDown = 1,
+    CRight = 2,
+    DPadUp = 3,
+    DPadDown = 4,
+    DPadLeft = 5,
+    DPadRight = 6,
+};
+
+static Gfx* Gfx_WideTextureRectCentered(Gfx* displayListHead, Vec3s center, s16 size, s16 texStep) {
+    s32 x = center.x - (size / 2);
+    s32 y = center.y - (size / 2);
+
+    gSPWideTextureRectangle(displayListHead++, x << 2, y << 2,
+                            (x + size) << 2,
+                            (y + size) << 2, G_TX_RENDERTILE, 0, 0,
+                            texStep << 1, texStep << 1);
+    return displayListHead;
+}
+
+static bool IsAmmoItem(s16 itemId) {
+    if ((itemId >= ITEM_BOW_ARROW_FIRE) && (itemId <= ITEM_BOW_ARROW_LIGHT)) {
+        return true;
+    }
+
+    switch (itemId) {
+        case ITEM_STICK:
+        case ITEM_NUT:
+        case ITEM_BOMB:
+        case ITEM_BOW:
+        case ITEM_SLINGSHOT:
+        case ITEM_BOMBCHU:
+        case ITEM_BEAN:
+            return true;
+        default:
+            return false;
+    }
+}
+
+static Gfx* DrawAmmoCountAt(Gfx* displayListHead, s16 itemId, s16 x, s16 y, s16 alpha) {
+    if (!IsAmmoItem(itemId)) {
+        return displayListHead;
+    }
+
+    if ((itemId >= ITEM_BOW_ARROW_FIRE) && (itemId <= ITEM_BOW_ARROW_LIGHT)) {
+        itemId = ITEM_BOW;
+    }
+
+    s16 ammo = AMMO(itemId);
+    if (ammo < 0) {
+        ammo = 0;
+    }
+
+    s16 tens = ammo / 10;
+    s16 ones = ammo % 10;
+
+    gDPSetPrimColor(displayListHead++, 0, 0, 255, 255, 255, alpha);
+    if (tens != 0) {
+        displayListHead = Gfx_TextureIA8(displayListHead, (u8*)_gAmmoDigit0Tex[tens], 8, 8, x, y, 8, 8, 1 << 10,
+                                         1 << 10);
+    }
+    displayListHead = Gfx_TextureIA8(displayListHead, (u8*)_gAmmoDigit0Tex[ones], 8, 8, x + 6, y, 8, 8, 1 << 10,
+                                     1 << 10);
+
+    return displayListHead;
+}
+
+void OnKaleidoUpdate() {
+    auto play = gPlayState;
+
+    if (play->pauseCtx.state == 6 && play->pauseCtx.pageIndex == PAUSE_ITEM) {
+        auto cursorItem = play->pauseCtx.cursorItem[PAUSE_ITEM];
+
+        if (cursorItem != PAUSE_ITEM_NONE) {
+            u32 ivanButtons = play->state.input[1].press.button;
+            if (cursorItem != ITEM_SOLD_OUT && cursorItem != ITEM_NONE) {
+                struct IvanEquipButton {
+                    u16 button;
+                    IvanItemIndex slot;
+                };
+                static const IvanEquipButton kEquipButtons[] = {
+                    { BTN_CLEFT, IvanItemIndex::CLeft },
+                    { BTN_CDOWN, IvanItemIndex::CDown },
+                    { BTN_CRIGHT, IvanItemIndex::CRight },
+                    { BTN_DUP, IvanItemIndex::DPadUp },
+                    { BTN_DDOWN, IvanItemIndex::DPadDown },
+                    { BTN_DLEFT, IvanItemIndex::DPadLeft },
+                    { BTN_DRIGHT, IvanItemIndex::DPadRight },
+                };
+
+                for (size_t i = 0; i < ARRAY_COUNT(kEquipButtons); i++) {
+                    if (CHECK_BTN_ALL(ivanButtons, kEquipButtons[i].button)) {
+                        gSaveContext.ship.ivanItems[(u8)kEquipButtons[i].slot] = cursorItem;
+                        Audio_PlaySoundGeneral(NA_SE_SY_DECIDE, &gSfxDefaultPos, 4, &gSfxDefaultFreqAndVolScale,
+                                               &gSfxDefaultFreqAndVolScale, &gSfxDefaultReverb);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+extern "C" void Ivan_DrawInventory() {
+    auto play = gPlayState;
+    auto interfaceCtx = &gPlayState->interfaceCtx;
+
+    OPEN_DISPS(play->state.gfxCtx);
+
+    gDPPipeSync(OVERLAY_DISP++);
+    gDPSetCombineMode(OVERLAY_DISP++, G_CC_MODULATEIA_PRIM, G_CC_MODULATEIA_PRIM);
+    gDPSetEnvColor(OVERLAY_DISP++, 0, 0, 0, 255);
+
+    Color_RGB8 primary = { 255, 255, 255 };
+    if (CVarGetInteger(CVAR_COSMETIC("Ivan.IdlePrimary.Changed"), 0)) {
+        primary = CVarGetColor24(CVAR_COSMETIC("Ivan.IdlePrimary.Value"), (Color_RGB8){ 255, 255, 255 });
+    }
+    Color_RGB8 secondary = { 0, 255, 0 };
+    if (CVarGetInteger(CVAR_COSMETIC("Ivan.IdleSecondary.Changed"), 0)) {
+        secondary = CVarGetColor24(CVAR_COSMETIC("Ivan.IdleSecondary.Value"), (Color_RGB8){ 0, 255, 0 });
+    }
+    Color_RGB8 tint = {
+        (u8)(((u16)primary.r + (u16)secondary.r) / 2),
+        (u8)(((u16)primary.g + (u16)secondary.g) / 2),
+        (u8)(((u16)primary.b + (u16)secondary.b) / 2),
+    };
+    gDPSetPrimColor(OVERLAY_DISP++, 0, 0, tint.r, tint.g, tint.b, 255);
+
+    Vec3s centers[4] = {
+        { (s16)(cButtonsCenterX - itemSpacing), cButtonsCenterY, 0 }, // C-Left
+        { cButtonsCenterX, (s16)(cButtonsCenterY + itemSpacing), 0 }, // C-Down
+        { (s16)(cButtonsCenterX + itemSpacing), cButtonsCenterY, 0 }, // C-Right
+        { cButtonsCenterX, (s16)(cButtonsCenterY - itemSpacing), 0 }, // C-Up
+    };
+
+    gDPLoadTextureBlock(OVERLAY_DISP++, gButtonBackgroundTex, G_IM_FMT_IA, G_IM_SIZ_8b, 32, 32, 0,
+                    G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMIRROR | G_TX_WRAP,
+                    G_TX_NOMASK, G_TX_NOMASK, G_TX_NOLOD, G_TX_NOLOD);
+    for (int i = 0; i < 4; i++)
+        OVERLAY_DISP = Gfx_WideTextureRectCentered(OVERLAY_DISP, centers[i], cButtonSize, cButtonTexStep);
+
+    Vec3s dpadCenters[4] = {
+        { dPadCenterX, (s16)(dPadCenterY - itemSpacing), 0 }, // Dpad-Up
+        { dPadCenterX, (s16)(dPadCenterY + itemSpacing), 0 }, // Dpad-Down
+        { (s16)(dPadCenterX - itemSpacing), dPadCenterY, 0 }, // Dpad-Left
+        { (s16)(dPadCenterX + itemSpacing), dPadCenterY, 0 }, // Dpad-Right
+    };
+
+    gDPLoadTextureBlock(OVERLAY_DISP++, gDPadTex, G_IM_FMT_IA, G_IM_SIZ_16b, dPadSize, dPadSize, 0,
+                        G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMIRROR | G_TX_WRAP,
+                        G_TX_NOMASK, G_TX_NOMASK, G_TX_NOLOD, G_TX_NOLOD);
+    OVERLAY_DISP = Gfx_WideTextureRectCentered(OVERLAY_DISP, { dPadCenterX, dPadCenterY, 0 }, dPadSize, dPadTexStep);
+
+    for (size_t i = 0; i < 4; i++) {
+        auto item = gSaveContext.ship.ivanItems[i + 3];
+        if (item == ITEM_NONE) {
+            continue;
+        }
+
+        gDPPipeSync(OVERLAY_DISP++);
+        void* texture = gItemIcons[item];
+        gDPLoadTextureBlock(OVERLAY_DISP++, texture, G_IM_FMT_RGBA, G_IM_SIZ_32b, 32, 32, 0,
+                            G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMIRROR | G_TX_WRAP,
+                            G_TX_NOMASK, G_TX_NOMASK, G_TX_NOLOD, G_TX_NOLOD);
+        gDPSetPrimColor(OVERLAY_DISP++, 0, 0, 255, 255, 255, 255);
+        OVERLAY_DISP = Gfx_WideTextureRectCentered(OVERLAY_DISP, dpadCenters[i], itemIconSize, itemIconTexStep);
+        OVERLAY_DISP =
+            DrawAmmoCountAt(OVERLAY_DISP, item, dpadCenters[i].x - (itemIconSize / 2),
+                            dpadCenters[i].y - (itemIconSize / 2), 255);
+    }
+
+    for (size_t i = 0; i < 3; i++) {
+        auto item = gSaveContext.ship.ivanItems[i];
+        if (item == ITEM_NONE) {
+            continue;
+        }
+
+        gDPPipeSync(OVERLAY_DISP++);
+        void* texture = gItemIcons[item];
+        gDPLoadTextureBlock(OVERLAY_DISP++, texture, G_IM_FMT_RGBA, G_IM_SIZ_32b, 32, 32, 0,
+                            G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMIRROR | G_TX_WRAP,
+                            G_TX_NOMASK, G_TX_NOMASK, G_TX_NOLOD, G_TX_NOLOD);
+        gDPSetPrimColor(OVERLAY_DISP++, 0, 0, 255, 255, 255, 255);
+        OVERLAY_DISP = Gfx_WideTextureRectCentered(OVERLAY_DISP, centers[i], itemIconSize, itemIconTexStep);
+        OVERLAY_DISP =
+            DrawAmmoCountAt(OVERLAY_DISP, item, centers[i].x - (itemIconSize / 2),
+                            centers[i].y - (itemIconSize / 2), 255);
+    }
+
+    {
+        s16 cUpLeftX = centers[3].x - (cButtonSize / 2);
+        s16 cUpLeftY = centers[3].y - (cButtonSize / 2);
+
+        gDPPipeSync(OVERLAY_DISP++);
+        gDPSetPrimColor(OVERLAY_DISP++, 0, 0, 255, 255, 255, 255);
+        gDPSetCombineLERP(OVERLAY_DISP++, PRIMITIVE, ENVIRONMENT, TEXEL0, ENVIRONMENT, TEXEL0, 0, PRIMITIVE, 0,
+                          PRIMITIVE, ENVIRONMENT, TEXEL0, ENVIRONMENT, TEXEL0, 0, PRIMITIVE, 0);
+        gDPLoadTextureBlock_4b(OVERLAY_DISP++, (void*)gNaviCUpENGTex, G_IM_FMT_IA, 32, 8, 0,
+                               G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMASK, G_TX_NOMASK,
+                               G_TX_NOLOD, G_TX_NOLOD);
+        gSPWideTextureRectangle(OVERLAY_DISP++, (cUpLeftX - naviLabelXOffset) << 2, (cUpLeftY + naviLabelYOffset) << 2,
+                                (cUpLeftX - naviLabelXOffset + 32) << 2, (cUpLeftY + naviLabelYOffset + 8) << 2,
+                                G_TX_RENDERTILE, (31 << 5), 0, -(1 << 10), 1 << 10);
+    }
+
+    gDPPipeSync(OVERLAY_DISP++);
+
+    CLOSE_DISPS(play->state.gfxCtx);
+}
+
+void RegisterIvanSeparateEquipSlots() {
+    COND_HOOK(OnKaleidoUpdate, true, OnKaleidoUpdate);
+}
+
+static RegisterShipInitFunc initFunc_Freezard(RegisterIvanSeparateEquipSlots);
