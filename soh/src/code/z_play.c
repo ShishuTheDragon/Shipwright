5759062e5759f8c9b1d152b51133f6ac0ab85ab9
Shishu the Dragon <183069616+ShishuTheDragon@users.noreply.github.com>
2026-05-01 15:10:34 +1200

    IvanSplitScreen: Don’t go into first person when Link does

--- soh/src/code/z_play.c
+++ soh/src/code/z_play.c
@@ -1438,6 +1438,7 @@ void Play_Draw(PlayState* play) {
                 play->view.lookAt.x = ivanPos.x;
                 play->view.lookAt.y = ivanPos.y + lookAtHeight;
                 play->view.lookAt.z = ivanPos.z;
+                play->view.fovy = 60.0f;
             }
         }
 
@@ -1746,6 +1747,9 @@ Play_Draw_skip:
 
     if (play->view.unk_124 != 0) {
         Camera_Update(GET_ACTIVE_CAM(play));
+        if (gIvanActor != NULL) {
+            play->view.viewingPtr = Graph_Alloc(gfxCtx, sizeof(Mtx));
+        }
         func_800AB944(&play->view);
         play->view.unk_124 = 0;
         if (play->skyboxId && (play->skyboxId != SKYBOX_UNSET_1D) && !play->envCtx.skyboxDisabled) {
