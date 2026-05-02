9e26236e18fba2e85d176fc2cdcbe6e639bc2da6
Shishu the Dragon <183069616+ShishuTheDragon@users.noreply.github.com>
2026-05-02 15:12:21 +1200

    IvanSplitScreen: Fix some billboards

--- soh/src/code/z_play.c
+++ soh/src/code/z_play.c
@@ -1492,6 +1492,7 @@ void Play_Draw(PlayState* play) {
             Matrix_MtxFToMtx(MATRIX_CHECKFLOATS(&play->billboardMtxF), Graph_Alloc(gfxCtx, sizeof(Mtx)));
 
         gSPSegment(POLY_OPA_DISP++, 0x01, play->billboardMtx);
+        gSPSegment(POLY_XLU_DISP++, 0x01, play->billboardMtx);
 
         if (splitPass == 0) {
         if ((HREG(80) != 10) || (HREG(92) != 0)) {
