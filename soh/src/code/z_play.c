1fbfd56474239d4014b79d23543b60250b4b23f9
Shishu the Dragon <183069616+ShishuTheDragon@users.noreply.github.com>
2026-05-08 16:25:50 +1200

    IvanSplitScreen: More zoomed in for Ivan’s viewport

--- soh/src/code/z_play.c
+++ soh/src/code/z_play.c
@@ -1431,7 +1431,7 @@ void Play_Draw(PlayState* play) {
                 play->view.up.y = 1.0f;
                 play->view.up.z = 0.0f;
                 Vec3f ivanPos = gIvanActor->actor.world.pos;
-                f32 camDist = 180.0f;
+                f32 camDist = 90.0f;
                 f32 lookAtHeight = 40.0f;
                 s16 yaw = (s16)gIvanCamYaw;
                 s16 pitch = (s16)gIvanCamPitch;
