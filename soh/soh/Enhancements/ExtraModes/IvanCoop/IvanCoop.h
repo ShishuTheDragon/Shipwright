2871d0663425fde21678cd7628456d2f74d12d8b
Shishu the Dragon <183069616+ShishuTheDragon@users.noreply.github.com>
2026-06-26 18:55:02 +1200

    fix rebase conflicts

--- soh/soh/Enhancements/ExtraModes/IvanCoop/IvanCoop.h
+++ soh/soh/Enhancements/ExtraModes/IvanCoop/IvanCoop.h
@@ -7,17 +7,5 @@ extern PlayState* gPlayState;
 extern s16 gEnPartnerId;
 }
 
-static inline EnPartner* GetIvanActor(PlayState* play) {
-    return (EnPartner*)Actor_Find(&play->actorCtx, gEnPartnerId, ACTORCAT_ITEMACTION);
-}
-
-static inline f32 XZDistToNearestPlayer(Actor* actor) {
-    EnPartner* ivan = GetIvanActor(gPlayState);
-    if (ivan != NULL) {
-        f32 distToIvan = Actor_WorldDistXZToActor(actor, &ivan->actor);
-        if (distToIvan < actor->xzDistToPlayer) {
-            return distToIvan;
-        }
-    }
-    return actor->xzDistToPlayer;
-}
+EnPartner* GetIvanActor(PlayState* play);
+f32 XZDistToNearestPlayer(Actor* actor);
